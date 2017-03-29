68e4c7943cc08d2b5dcb898d46688cc01ed44407
Essi Hagman <Essi.Hagman@Gmail.com>
2017-03-29 15:33:38 +0200

    position/direction stuff

--- Bash/Model/Bash.cs
+++ Bash/Model/Bash.cs
@@ -8,22 +8,69 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Bash
 {
-	public class Position
+	public class Vec2i
 	{
 		public int X;
 		public int Y;
 
-		public Position(int x, int y)
+		public Vec2i(int x, int y)
 		{
 			this.X = x;
 			this.Y = y;
+		}
+
+		public static Vec2i operator +(Vec2i a, Vec2i b)
+		{
+			return new Vec2i(a.X + b.X, a.Y + b.Y);
+		}
+	}
+
+	public class Position
+	{
+		public Vec2i V;
+		public int X => this.V.X;
+		public int Y => this.V.Y;
+
+		public Position(int x, int y)
+			: this(new Vec2i(x, y))
+		{
+		}
+
+		public Position(Vec2i position)
+		{
+			this.V = position;
+		}
+
+		public static Position operator +(Position p, Position q)
+		{
+			return new Position(p.V + q.V);
+		}
+
+		public static Position operator +(Position p, Direction q)
+		{
+			return new Position(p.V + q.V);
 		}
 	}
 
 	public class Direction
 	{
-		public Direction(int dx, int dy)
+		public Vec2i V;
+		public int Dx => this.V.X;
+		public int Dy => this.V.Y;
+
+		public Direction(int x, int y)
+			: this(new Vec2i(x, y))
 		{
+		}
+
+		public Direction(Vec2i position)
+		{
+			this.V = position;
+		}
+
+		public static Direction Normalise(Direction d)
+		{
+			throw new System.NotImplementedException();
 		}
 	}
 
@@ -41,9 +88,8 @@ namespace Bash
 
 		public override void ApplyTo(Character character)
 		{
-			throw new NotImplementedException();
-
-			var terrain = character.Map.Terrain.GetAt(character.Position);
+			var fromTerrain = character.Map.Terrain.GetAt(character.Position);
+			var toTerrain = character.Map.Terrain.GetAt(character.Position + direction);
 		}
 	}
 
@@ -54,6 +100,7 @@ namespace Bash
 
 	public class Terrain
 	{
+		public int Elevation; // 0 = sea level
 	}
 
 	// This is our model class,
